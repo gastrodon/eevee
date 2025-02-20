377d35ada71e4fa15ee8a26286ceb33af3c4281a
eva <mail@gastrodon.io>
2025-02-19 21:55:09 -0500

    try to do some training and specie sizing
    specie_population calculates a map specie -> target population, which
    is its proportion of that species fitness to total fitness, relative to
    POPULATION
    
    specie_sizer adjusts that calculation in order to only keep top_p%
    members of the population
    
    Some code, right now haphazardly thrown into a loop in main, about
    training a number of GENERATIONS against xor evaluator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use rand::{rng, rngs::ThreadRng};
 use specie::{speciate, InnoGen, Specie, SpecieRepr};
 use std::collections::HashMap;
 
+const GENERATIONS: usize = 1000;
 const POPULATION: usize = 100;
 const FIT_SCALE: usize = 512;
 
@@ -26,20 +27,42 @@ fn population_init(
     v
 }
 
+fn specie_sizer(population: usize, top_p: f64) -> Box<dyn Fn(f64, f64) -> usize> {
+    let pop_adj = population as f64 / top_p;
+    Box::new(move |fit_1, fit_total| (pop_adj * fit_1 / fit_total).round() as usize)
+}
+
+// drop_p is a whole percent
 fn specie_populations<'a>(
     species: &'a Vec<Specie<'a>>,
     population: usize,
+    top_p: f64,
 ) -> HashMap<&'a SpecieRepr<'a>, usize> {
-    let fits = species
+    let mut fits = species
         .iter()
         .map(|s| (&s.0, s.fit_adjusted()))
-        .collect::<HashMap<_, _>>();
+        .collect::<Vec<_>>();
 
-    let population = population as f64;
-    let fit_total = fits.values().sum::<f64>();
-    fits.into_iter()
-        .map(|(s, f)| (s, (population * (f / fit_total)) as usize))
-        .collect()
+    // I speculate partial_cmp may be none if the fitness is NaN,
+    // which would indicate a bigger issue somewhere else
+    fits.sort_by(|(_, l), (_, r)| r.partial_cmp(l).unwrap());
+
+    let sizer = specie_sizer(population, top_p);
+    let fit_total = fits.iter().fold(0., |acc, (_, n)| acc + n);
+
+    let mut sizes = HashMap::new();
+    let mut size_acc = 0;
+    for (s_repr, fit) in fits.into_iter() {
+        let s_size = sizer(fit, fit_total);
+        if size_acc + s_size < population {
+            sizes.insert(s_repr, s_size);
+            size_acc += s_size;
+        } else {
+            sizes.insert(s_repr, population - size_acc);
+            break;
+        }
+    }
+    sizes
 }
 
 fn main() {
@@ -50,36 +73,49 @@ fn main() {
     let mut inno_head = 0;
 
     let mut population = population_init(2, 1, POPULATION, &mut rng);
-    for gen_idx in 0..8 {
-        println!("gen {gen_idx}...");
-
+    let mut gen_idx = 0;
+    let pop_evaluated = loop {
         let scored = population
             .iter()
             .map(|genome| {
-                let fit = genome.propagate_game(&mut GameXOR::new(FIT_SCALE), true);
+                let fit = genome.propagate_game(&mut GameXOR::new(FIT_SCALE), false);
                 (genome, fit / FIT_SCALE)
             })
             .collect::<Vec<_>>();
 
-        let species = speciate(scored.into_iter());
-        let species_pop = specie_populations(&species, POPULATION);
+        let species = {
+            let mut species = speciate(scored.into_iter());
+            for s in species.iter_mut() {
+                s.shrink_top_p(0.01);
+            }
+            species
+        };
 
-        //
-        dbg!(species
-            .iter()
-            .map(|specie| (specie.fit_adjusted(), species_pop.get(&specie.0).unwrap()))
-            .collect::<Vec<_>>());
-        //
+        let species_pop = specie_populations(&species, POPULATION, 0.5);
+        if gen_idx == GENERATIONS {
+            break species;
+        }
 
         let mut innogen = InnoGen::new(inno_head);
         population = species
             .iter()
             .flat_map(|specie| {
                 specie
-                    .reproduce(*species_pop.get(&specie.0).unwrap(), &mut innogen, &mut rng)
+                    .reproduce(
+                        *species_pop.get(&specie.0).unwrap_or(&0),
+                        &mut innogen,
+                        &mut rng,
+                    )
                     .unwrap()
             })
             .collect::<Vec<_>>();
-        inno_head = dbg!(innogen.head);
+        inno_head = innogen.head;
+
+        gen_idx += 1;
+    };
+
+    println!("{gen_idx}");
+    for (idx, s) in pop_evaluated.iter().filter(|s| !s.is_empty()).enumerate() {
+        println!("champ {idx}: {}", s.1[0].1)
     }
 }
