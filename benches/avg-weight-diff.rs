7e3d85e892576fb8dfe95f185eedea4755d0aca9
eva <mail@gastrodon.io>
2025-03-03 00:12:02 -0500

    no-hash weight-diff and disjoint-excess counters
    Since we are sorting connections by inno id when we generate them in
    crossover, we know that by the time they get to avg-weight-diff and
    disjoint-excess-count they'll be in order. Because of this.
    We can therefore skip all hashing / set operations, and iter each pair
    once.
    
    We walk through the l and r connecitons at the same time. If either
    yields nothing, we know that we're done getting weight diffs. We find
    each next overlapping inno id and track their absolute difference.
    
    While I'm at it, make the weight-diff tests print the diff if it's
    not what we expect

---
+++ benches/avg-weight-diff.rs
@@ -0,0 +1,35 @@
+use brain::crossover::avg_weight_diff;
+use criterion::Criterion;
+fn bench(bench: &mut Criterion) {
+    let l_conn = serde_json::from_str::<Vec<_>>(include_str!("connection-rand-l.json")).unwrap();
+    let r_conn = serde_json::from_str::<Vec<_>>(include_str!("connection-rand-r.json")).unwrap();
+
+    bench.bench_function("avg-weight-diff", |b| {
+        b.iter(|| avg_weight_diff(&l_conn, &r_conn))
+    });
+}
+
+pub fn benches() {
+    #[cfg(not(feature = "smol_bench"))]
+    let mut criterion: criterion::Criterion<_> = Criterion::default()
+        .sample_size(1000)
+        .significance_level(0.1);
+    #[cfg(feature = "smol_bench")]
+    let mut criterion: criterion::Criterion<_> = {
+        use std::time::Duration;
+        Criterion::default()
+            .measurement_time(Duration::from_millis(1))
+            .sample_size(10)
+            .nresamples(1)
+            .without_plots()
+            .configure_from_args()
+    };
+    bench(&mut criterion);
+}
+
+fn main() {
+    benches();
+    criterion::Criterion::default()
+        .configure_from_args()
+        .final_summary();
+}
