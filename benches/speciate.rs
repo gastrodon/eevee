709f8d60a65d4ae147d7c296678d94504dd044f5
eva <mail@gastrodon.io>
2025-03-08 12:18:39 -0500

    a benchmark for speciation

---
+++ benches/speciate.rs
@@ -0,0 +1,31 @@
+use brain::specie::speciate;
+use criterion::Criterion;
+fn bench(bench: &mut Criterion) {
+    let genomes = serde_json::from_str::<Vec<_>>(include_str!("genome-xor-100.json")).unwrap();
+    bench.bench_function("speciate", |b| b.iter(|| speciate(genomes.clone(), vec![])));
+}
+
+pub fn benches() {
+    #[cfg(not(feature = "smol_bench"))]
+    let mut criterion: criterion::Criterion<_> = Criterion::default()
+        .sample_size(1000)
+        .significance_level(0.1);
+    #[cfg(feature = "smol_bench")]
+    let mut criterion: criterion::Criterion<_> = {
+        use std::time::Duration;
+        Criterion::default()
+            .measurement_time(Duration::from_millis(1))
+            .sample_size(10)
+            .nresamples(1)
+            .without_plots()
+            .configure_from_args()
+    };
+    bench(&mut criterion);
+}
+
+fn main() {
+    benches();
+    criterion::Criterion::default()
+        .configure_from_args()
+        .final_summary();
+}
