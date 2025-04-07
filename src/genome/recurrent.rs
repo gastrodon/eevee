22a66c31dda6039ecad17617e3bf06f9bdb548bb
eva <mail@gastrodon.io>
2025-04-06 22:07:17 -0400

    Move towards generic tests

--- src/genome/recurrent.rs
+++ src/genome/recurrent.rs
@@ -135,61 +135,72 @@ impl<N: Node, C: Connection<N>> Genome<N, C> for Recurrent<N, C> {
 mod test {
     use super::*;
     use crate::{
-        genome::{
-            node::{BTNode, NonBNode},
-            Biased, WConnection,
-        },
+        genome::{node::BTNode, WConnection},
         random::default_rng,
         specie::InnoGen,
+        test_t,
     };
 
-    #[test]
-    fn test_genome_creation() {
-        let (genome, inno_head) = Recurrent::<BTNode, WConnection<BTNode>>::new(3, 2);
+    type N = BTNode;
+    type C = WConnection<N>;
+    type RecurrentContinuous = Recurrent<N, C>;
+
+    test_t!(
+    test_genome_creation[T: RecurrentContinuous]() {
+        let (genome, inno_head) = T::new(3, 2);
         assert_eq!(inno_head, 8);
-        assert_eq!(genome.sensory, 3);
-        assert_eq!(genome.action, 2);
-        assert_eq!(genome.nodes.len(), 6);
-        assert!(matches!(genome.nodes[0], NonBNode::Sensory));
-        assert!(matches!(genome.nodes[3], NonBNode::Action));
-        assert!(matches!(genome.nodes[5], NonBNode::Static(_)));
+        assert_eq!(genome.sensory().len(), 3);
+        assert_eq!(genome.action().len(), 2);
+        assert_eq!(genome.nodes().len(), 6);
+        assert!(matches!(genome.nodes[0].kind(), NodeKind::Sensory));
+        assert!(matches!(genome.nodes[3].kind(), NodeKind::Action));
+        assert!(matches!(genome.nodes[5].kind(), NodeKind::Static));
+    });
 
-        let (genome_empty, inno_head) = Recurrent::<BTNode, WConnection<BTNode>>::new(0, 0);
+    test_t!(
+    test_genome_creation_empty[T: RecurrentContinuous]() {
+        let (genome, inno_head) = T::new(0, 0);
         assert_eq!(inno_head, 0);
-        assert_eq!(genome_empty.sensory, 0);
-        assert_eq!(genome_empty.action, 0);
-        assert_eq!(genome_empty.nodes.len(), 1);
-        assert!(matches!(genome_empty.nodes[0], NonBNode::Static(_)));
+        assert_eq!(genome.sensory().len(), 0);
+        assert_eq!(genome.action().len(), 0);
+        assert_eq!(genome.nodes().len(), 1);
+        assert!(matches!(genome.nodes()[0].kind(), NodeKind::Static));
+    });
 
-        let (genome_only_sensory, inno_head) = Recurrent::<BTNode, WConnection<BTNode>>::new(3, 0);
+    test_t!(
+    test_genome_creation_only_sensory[T: RecurrentContinuous]() {
+        let (genome, inno_head) = T::new(3, 0);
         assert_eq!(inno_head, 0);
-        assert_eq!(genome_only_sensory.sensory, 3);
-        assert_eq!(genome_only_sensory.action, 0);
-        assert_eq!(genome_only_sensory.nodes.len(), 4);
-        assert!(matches!(genome_only_sensory.nodes[0], NonBNode::Sensory));
-        assert!(matches!(genome_only_sensory.nodes[2], NonBNode::Sensory));
-        assert!(matches!(genome_only_sensory.nodes[3], NonBNode::Static(_)));
+        assert_eq!(genome.sensory().len(), 3);
+        assert_eq!(genome.action().len(), 0);
+        assert_eq!(genome.nodes().len(), 4);
+        assert!(matches!(genome.nodes()[0].kind(), NodeKind::Sensory));
+        assert!(matches!(genome.nodes()[2].kind(), NodeKind::Sensory));
+        assert!(matches!(genome.nodes()[3].kind(), NodeKind::Static));
+    });
 
-        let (genome_only_action, inno_head) = Recurrent::<BTNode, WConnection<BTNode>>::new(0, 3);
+    test_t!(
+    test_genome_creation_only_action[T: RecurrentContinuous]() {
+        let (genome, inno_head) = T::new(0, 3);
         assert_eq!(inno_head, 3);
-        assert_eq!(genome_only_action.sensory, 0);
-        assert_eq!(genome_only_action.action, 3);
-        assert_eq!(genome_only_action.nodes.len(), 4);
-        assert!(matches!(genome_only_action.nodes[0], NonBNode::Action));
-        assert!(matches!(genome_only_action.nodes[2], NonBNode::Action));
-        assert!(matches!(genome_only_action.nodes[3], NonBNode::Static(_)));
-    }
+        assert_eq!(genome.sensory().len(), 0);
+        assert_eq!(genome.action().len(), 3);
+        assert_eq!(genome.nodes().len(), 4);
+        assert!(matches!(genome.nodes()[0].kind(), NodeKind::Action));
+        assert!(matches!(genome.nodes()[2].kind(), NodeKind::Action));
+        assert!(matches!(genome.nodes()[3].kind(), NodeKind::Static));
+    });
 
-    #[test]
-    fn test_gen_connection() {
+    test_t!( // TODO fixme: we are allowing connectinons to sensory + from bias
+    test_gen_connection[T: RecurrentContinuous]() {
         let mut inno = InnoGen::new(0);
-        let genome = Recurrent {
+        let genome = T {
             sensory: 1,
             action: 1,
-            nodes: vec![NonBNode::Sensory, NonBNode::Action],
+            nodes: vec![N::new(NodeKind::Sensory), N::new(NodeKind::Action)],
             connections: vec![
-                WConnection::<NonBNode>::new(0, 0, &mut inno),
-                WConnection::<NonBNode>::new(1, 1, &mut inno),
+                C::new(0, 0, &mut inno),
+                C::new(1, 1, &mut inno),
             ],
         };
         for _ in 0..100 {
@@ -199,75 +210,47 @@ mod test {
                 None => unreachable!("no path gen'd"),
             }
         }
-    }
+    });
 
-    #[test]
-    fn test_gen_connection_no_dupe() {
+    test_t!( // TODO fixme: we are allowing connections to sensory
+    test_gen_connection_no_dupe[T: RecurrentContinuous]() {
         let mut inno = InnoGen::new(0);
-        let genome = Recurrent {
+        let genome = T {
             sensory: 1,
             action: 1,
-            nodes: vec![NonBNode::Sensory, NonBNode::Action],
+            nodes: vec![N::new(NodeKind::Sensory), N::new(NodeKind::Action)],
             connections: vec![
-                WConnection::<NonBNode>::new(0, 0, &mut inno),
-                WConnection::<NonBNode>::new(0, 1, &mut inno),
-                WConnection::<NonBNode>::new(1, 1, &mut inno),
+                C::new(0, 0, &mut inno),
+                C::new(0, 1, &mut inno),
+                C::new(1, 1, &mut inno),
             ],
         };
         for _ in 0..100 {
             assert_eq!(genome.open_path(&mut default_rng()), Some((1, 0)));
         }
-    }
+    });
 
-    #[test]
-    fn test_gen_connection_none_possble() {
+    test_t!( // TODO fixme: we are allowing connections to sensory
+    test_gen_connection_none_possible[T: RecurrentContinuous]() {
         let mut inno = InnoGen::new(0);
         assert_eq!(
-            Recurrent::<NonBNode, WConnection<NonBNode>> {
+            T {
                 sensory: 0,
                 action: 0,
                 nodes: vec![],
-                connections: vec![WConnection::<NonBNode>::new(0, 1, &mut inno)],
+                connections: vec![C::new(0, 1, &mut inno)],
             }
             .open_path(&mut default_rng()),
             None
         );
-    }
+    });
 
-    #[test]
-    fn test_gen_connection_saturated() {
-        assert_eq!(
-            Recurrent {
-                sensory: 2,
-                action: 2,
-                nodes: vec![
-                    NonBNode::Action,
-                    NonBNode::Action,
-                    NonBNode::Sensory,
-                    NonBNode::Sensory,
-                    NonBNode::Static(1.),
-                ],
-                connections: (0..5)
-                    .flat_map(|from| {
-                        (0..5).map(move |to| {
-                            WConnection::<NonBNode>::new(from, to, &mut InnoGen::new(0))
-                        })
-                    })
-                    .collect(),
-            }
-            .open_path(&mut default_rng()),
-            None
-        )
-    }
-
-    #[test]
-    fn test_mutate_connection() {
-        let (mut genome, _) = Recurrent::<NonBNode, WConnection<NonBNode>>::new(4, 4);
+    test_t!(
+    test_mutate_connection[T: RecurrentContinuous]() {
+        let (mut genome, _) = T::new(4, 4);
         let mut inno = InnoGen::new(0);
-        genome.push_2_connections(
-            WConnection::<NonBNode>::new(0, 1, &mut inno),
-            WConnection::<NonBNode>::new(1, 2, &mut inno),
-        );
+        genome.push_connection(C::new(0, 1, &mut inno));
+        genome.push_connection(C::new(1, 2, &mut inno));
 
         let before = genome.clone();
         genome.new_connection(&mut default_rng(), &mut inno);
@@ -278,15 +261,15 @@ mod test {
         assert!(!before.connections().iter().any(|c| c.inno() == tail.inno()));
         assert!(!before.connections().iter().any(|c| c.path() == tail.path()));
         assert_eq!(tail.weight(), 1.);
-    }
+    });
 
-    #[test]
-    fn test_mutate_bisection() {
+    test_t!(
+    test_mutate_bisection[T: RecurrentContinuous]() {
         let mut inno = InnoGen::new(0);
-        let (mut genome, _) = Recurrent::<NonBNode, WConnection<NonBNode>>::new(0, 1);
+        let (mut genome, _) = T::new(0, 1);
 
         genome.push_connection({
-            let mut c = WConnection::<NonBNode>::new(0, 1, &mut inno);
+            let mut c = C::new(0, 1, &mut inno);
             c.mutate_param(&mut default_rng());
             c
         });
@@ -294,48 +277,47 @@ mod test {
         let innogen = &mut InnoGen::new(1);
         genome.bisect_connection(&mut default_rng(), innogen);
 
-        let connections: &[WConnection<NonBNode>] = genome.connections();
-        assert!(!connections[0].enabled);
+        assert!(!genome.connections()[0].enabled);
 
-        assert_eq!(connections[1].from(), 0);
-        assert_eq!(connections[1].to(), 2);
-        assert_eq!(connections[1].weight(), 1.0);
-        assert!(connections[1].enabled);
+        assert_eq!(genome.connections()[1].from(), 0);
+        assert_eq!(genome.connections()[1].to(), 2);
+        assert_eq!(genome.connections()[1].weight(), 1.0);
+        assert!(genome.connections()[1].enabled);
         assert_eq!(
-            genome.connections[1].inno,
-            innogen.path((genome.connections[1].from(), genome.connections[1].to()))
+            genome.connections()[1].inno,
+            innogen.path((genome.connections()[1].from(), genome.connections()[1].to()))
         );
 
-        assert_eq!(genome.connections[2].from(), 2);
-        assert_eq!(genome.connections[2].to(), 1);
-        assert_eq!(genome.connections[1].weight(), 1.);
+        assert_eq!(genome.connections()[2].from(), 2);
+        assert_eq!(genome.connections()[2].to(), 1);
+        assert_eq!(genome.connections()[1].weight(), 1.);
         assert_eq!(
-            genome.connections[2].weight(),
-            genome.connections[0].weight()
+            genome.connections()[2].weight(),
+            genome.connections()[0].weight()
         );
-        assert!(genome.connections[2].enabled);
+        assert!(genome.connections()[2].enabled);
         assert_eq!(
-            genome.connections[2].inno,
-            innogen.path((genome.connections[2].from(), genome.connections[2].to()))
+            genome.connections()[2].inno,
+            innogen.path((genome.connections()[2].from(), genome.connections()[2].to()))
         );
 
-        assert_ne!(genome.connections[0].inno, genome.connections[1].inno);
-        assert_ne!(genome.connections[1].inno, genome.connections[2].inno);
-        assert_ne!(genome.connections[0].inno, genome.connections[2].inno);
-    }
+        assert_ne!(genome.connections()[0].inno, genome.connections()[1].inno);
+        assert_ne!(genome.connections()[1].inno, genome.connections()[2].inno);
+        assert_ne!(genome.connections()[0].inno, genome.connections()[2].inno);
+    });
 
-    #[test]
-    #[should_panic]
-    fn test_mutate_bisection_empty_genome() {
-        let (mut genome, _) = Recurrent::<NonBNode, WConnection<NonBNode>>::new(0, 0);
+    test_t!(
+    #[should_panic(expected = "no connections available to bisect")]
+    test_mutate_bisection_empty_genome[T: RecurrentContinuous]() {
+        let (mut genome, _) = T::new(0, 0);
         genome.bisect_connection(&mut default_rng(), &mut InnoGen::new(0));
-    }
+    });
 
-    #[test]
-    #[should_panic]
-    fn test_mutate_bisection_no_connections() {
-        let (mut genome, _) = Recurrent::<NonBNode, WConnection<NonBNode>>::new(2, 2);
+    test_t!(
+    #[should_panic(expected = "no connections available to bisect")]
+    test_mutate_bisection_no_connections[T: RecurrentContinuous]() {
+        let (mut genome, _) = T::new(2, 2);
         genome.connections = vec![];
         genome.bisect_connection(&mut default_rng(), &mut InnoGen::new(0));
-    }
+    });
 }
